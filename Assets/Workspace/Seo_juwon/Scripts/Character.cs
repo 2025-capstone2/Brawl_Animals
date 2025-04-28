443eb97df6d5a1eb3330ea47b7bea4aaa2ca0f42
SeafoodBibimbap <155558935+SeafoodBibimbap@users.noreply.github.com>
2025-04-28 15:29:28 +0900

    change

--- Assets/Workspace/Seo_juwon/Scripts/Character.cs
+++ Assets/Workspace/Seo_juwon/Scripts/Character.cs
@@ -4,25 +4,22 @@ public class Character : MonoBehaviour
 {
     public string characterName;
     public int maxHp = 1000;
-    public int currentHp = 1000;
+    public int currentHp;
 
     public Skill skill;
     public Transform firePoint;
 
     private void Start()
     {
-        //시작할 때 hp는 max
         currentHp = maxHp;
         Debug.Log($"{characterName} HP: {currentHp}");
     }
 
-    //피해를 입었을 때
     public void TakeDamage(int amount)
     {
-        //현재 hp에서 amount만큼 깎임
         currentHp -= amount;
         Debug.Log($"{characterName} 피해 입음: {amount}, 남은 HP: {currentHp}");
-        //hp가 0이 되면 사망
+
         if (currentHp <= 0)
         {
             Die();
